69eb03a4eb1c3226f58ce0ababfda133e7385d0e
Brayzen <brian.ray.biz@gmail.com>
2015-08-01 19:22:49 -0700

    Chaning over from sqlite3 to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150730002931) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "robots", force: :cascade do |t|
     t.string   "name"
     t.string   "creator"
